c2ea2b33d900088ae04b0ebf1d1efbc6424264c7
EduardoBDonovan <eddiemb26@gmail.com>
2025-02-07 01:32:09 -0800

    Save State

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,9 @@ import ConFront from "../images/Con_Front.jpg";
 import ConSide from "../images/Con_Side.jpg";
 import ConFood from "../images/Con_Food.jpg";
 
+import { SiTiktok, SiInstagram } from "react-icons/si";
+import { FiPhone, FiMail } from "react-icons/fi";
+
 const HomePage = () => {
   return (
     <div className="homepage" id="home">
@@ -18,7 +21,10 @@ const HomePage = () => {
       >
         <div className="split-text">
           <div className="top-third">
-            <h1>Experiences by Connor McGlynn</h1>
+            <h1>
+              Experiences by
+              <br className="mobile-break" /> Connor McGlynn
+            </h1>
           </div>
           <div className="middle-third">{/* Intentionally left blank */}</div>
           <div className="bottom-third">
@@ -61,21 +67,38 @@ const HomePage = () => {
         style={{ backgroundImage: `url(${ConFood})` }}
       >
         <div className="split-text">
-          <div className="top-third">
-            <h2>Seasonal Curated Menus</h2>
-          </div>
+          <div className="top-third"></div>
           <div className="middle-third">
             <h2>Seasonal Curated Menus</h2>
           </div>
-          <div className="bottom-third">
-            <h2>Seasonal Curated Menus</h2>
-          </div>
+          <div className="bottom-third"></div>
         </div>
       </section>
 
       {/* Footer */}
       <footer className="footer">
-        <p>Footer Content</p>
+        <div className="footer-container">
+          <a
+            href="https://www.tiktok.com/@caviarcon?_t=8qDxp0BJ4yl&_r=1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiTiktok />
+          </a>
+          <a
+            href="https://instagram.com/connormcglynn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiInstagram />
+          </a>
+          <a href="tel:7149166951">
+            <FiPhone />
+          </a>
+          <a href="mailto:connor@caviarcon.com">
+            <FiMail />
+          </a>
+        </div>
       </footer>
     </div>
   );
