c92735ff8cf31dac6857f1f550ded1c14734ae37
EduardoBDonovan <eddiemb26@gmail.com>
2025-02-12 23:03:55 -0800

    Added more recipes to gallery page

--- src/components/GalleryPage.jsx
+++ src/components/GalleryPage.jsx
@@ -3,6 +3,13 @@ import "../styles/GalleryPage.css";
 import PulpoYPapas from "../images/gallerypics/PulpoYPapas.jpeg";
 import PokeTacos from "../images/gallerypics/Poke Tacos.jpg";
 import CremeBrulee from "../images/gallerypics/Creme Brulee.jpeg";
+import DuckBreast from "../images/gallerypics/Duck_Breast_&_Beet_Puree.jpg";
+import PestoSpagh from "../images/gallerypics/Pesto_Spaghetti_&_Torched_Salmon.jpg";
+import PotatoPave from "../images/gallerypics/Potato_Pave_with_Smoked_Egg_Yolk_&_Caviar.jpg";
+import RosePetalTort from "../images/gallerypics/Rose Petal Tortellini.jpeg";
+import SalmonTartare from "../images/gallerypics/Salmon_Tartare.jpg";
+import ScallopCrudo from "../images/gallerypics/Scallop_Crudo_w_Miso_Beurre_Blanc_&_Caviar.jpg";
+import SpicyPesto from "../images/gallerypics/Spicy Pesto Bucatini.jpg";
 import FlashCard from "./Flashcard"; // Import the flash card component
 import useTextFile from "../hooks/useTextFile";
 
@@ -35,6 +42,76 @@ const GalleryPage = () => {
           frontText="Hello, Front Side!"
           backText={pbcbRecipe}
           backgroundImage={CremeBrulee}
+        />
+      </section>
+      <section className="gallery-item">
+        <FlashCard
+          id="flashcard"
+          frontText="Hello, Front Side!"
+          backText="Duck Breast & Beet Puree 
+          
+          Recipe Coming Soon"
+          backgroundImage={DuckBreast}
+        />
+      </section>
+      <section className="gallery-item">
+        <FlashCard
+          id="flashcard"
+          frontText="Hello, Front Side!"
+          backText="Pesto Spaghetti & Torched Salmon 
+          
+          Recipe Coming Soon"
+          backgroundImage={PestoSpagh}
+        />
+      </section>
+      <section className="gallery-item">
+        <FlashCard
+          id="flashcard"
+          frontText="Hello, Front Side!"
+          backText="Potato Pave with Smoked Egg Yolk & Caviar 
+          
+          Recipe Coming Soon"
+          backgroundImage={PotatoPave}
+        />
+      </section>
+      <section className="gallery-item">
+        <FlashCard
+          id="flashcard"
+          frontText="Hello, Front Side!"
+          backText="Rose Petal Tortellini 
+          
+          Recipe Coming Soon"
+          backgroundImage={RosePetalTort}
+        />
+      </section>
+      <section className="gallery-item">
+        <FlashCard
+          id="flashcard"
+          frontText="Hello, Front Side!"
+          backText="Salmon Tartare 
+          
+          Recipe Coming Soon"
+          backgroundImage={SalmonTartare}
+        />
+      </section>
+      <section className="gallery-item">
+        <FlashCard
+          id="flashcard"
+          frontText="Hello, Front Side!"
+          backText="Scallop Crudo with Miso Beurre Blanc & Caviar 
+          
+          Recipe Coming Soon"
+          backgroundImage={ScallopCrudo}
+        />
+      </section>
+      <section className="gallery-item">
+        <FlashCard
+          id="flashcard"
+          frontText="Hello, Front Side!"
+          backText="Spicy Pesto Bucatini 
+          
+          Recipe Coming Soon"
+          backgroundImage={SpicyPesto}
         />
       </section>
     </div>
