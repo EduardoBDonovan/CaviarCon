b9773dc1ad6d0a9e2d23ef738b4f475f1ac5bac4
EduardoBDonovan <eddiemb26@gmail.com>
2025-02-14 12:15:06 -0800

    Not gonna say final butttt

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/CaviarCon/">
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
